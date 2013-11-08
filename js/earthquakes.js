d6e21b608177b640412f3dad03d0a3008431a764
unknown <iguest@IS-M334L23-17.ischool.uw.edu>
2013-11-08 12:19:58 -0800

    inclassLab7

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -5,6 +5,8 @@
     app token (pass as '$$app_token' query string param): Hwu90cjqyFghuAWQgannew7Oi
 */
 
+//  https://soda.demo.socrata.com/resource/earthquakes.json?$$app_token=Hwu90cjqyFghuAWQgannew7Oi
+
 //create a global variable namespace based on usgs.gov
 //this is how JavaScript developers keep global variables
 //separate from one another when mixing code from different
@@ -30,3 +32,66 @@ $(document).ajaxError(function(event, jqXHR, err){
     alert('Problem obtaining data: ' + jqXHR.statusText);
 });
 
+$(function(){
+        getQuakes();
+});
+
+function getQuakes() {
+	$.getJSON(gov.usgs.quakesUrl, function(quakes){
+	    //quakes is an array of objects, each of which represents info about a quake
+	    //see data returned from:
+	    //https://soda.demo.socrata.com/resource/earthquakes.json?$$app_token=Hwu90cjqyFghuAWQgannew7Oi
+
+	    //set our global variable to the current set of quakes
+	    //so we can reference it later in another event
+	    gov.usgs.quakes = quakes;
+	    $('.message').html('Displaying' + quakes.length + 'earthquakes');
+	    addQuakeMarkers(gov.usgs.quakes, map);
+
+	}); //handle returned data function
+
+		
+		map = new google.maps.Map($('.map-container')[0], {
+	    center: new google.maps.LatLng(0, 0),
+	    zoom: 2,
+	    mapTypeId: google.maps.MapTypeId.TERRAIN,
+	    streetViewControl: false
+	});
+	
+} //getQuakes()
+
+function addQuakeMarkers(quakes, map) {
+    
+    //loop over the quakes array and add a marker for each quake
+    var quake;      //current quake data
+    var idx;        //loop counter
+
+    for (idx = 0; idx < quakes.length; ++idx) {
+        quake = quakes[idx];
+
+        if(quake.location){
+		    quake.mapMarker = new google.maps.Marker({
+			    map: map,
+			    position: new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
+			});
+		}
+
+		google.maps.event.addListener(quake.mapMarker, 'click', function(){
+			if (gov.usgs.iw) {
+				gov.usgs.iw.close();
+			}
+    		//code that runs when user clicks on a marker
+    		gov.usgs.iw = new google.maps.InfoWindow({
+	   		content: new Date(quake.datetime).toLocaleString() + 
+	        ': magnitude ' + quake.magnitude + ' at depth of ' + 
+	
+	        quake.depth + ' meters'
+			});
+
+			//open the info window
+			gov.usgs.iw.open(map, this);
+		});
+    }
+
+
+} //addQuakeMarkers()
